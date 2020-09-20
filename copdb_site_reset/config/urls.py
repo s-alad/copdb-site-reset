91ec9da8f4ce794dee14acee21654bb31958afdf
ssaladd <n7.saad@gmail.com>
2020-09-19 21:46:10 -0400

    aa

---
+++ copdb_site_reset/config/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')), # new
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+]
